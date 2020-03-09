285b8a437a106f49c8be6bafad89a8e7aefca922
Eldar <eldar.works@gmail.com>
2020-03-09 23:31:44 +0700

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 import Main from '../main/main.jsx';
 
+const movies = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`];
+
 const App = () => {
-  return <Main />;
+  return <Main
+    movies={movies}
+  />;
 };
 
 export default App;
